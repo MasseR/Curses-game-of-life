2fb1ffe545b69fe8f610e73f056a1767bd0e1958
Mats Rauhala <mats.rauhala@gmail.com>
2009-10-02 19:59:14 +0300

    Comment the code

--- life.c
+++ life.c
@@ -1,3 +1,4 @@
+/* XXX: Toggle the cells */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,38 +8,68 @@
 #include "macros.h"
 
 
+/* Array to hold the cells */
 static int *cells = NULL;
+/* Array to hold the cells of the next round */
 static int *buffer = NULL;
+
+/* Size of the 'game area' */
 static int lifecols = 0;
 static int lifelines = 0;
+
+/* Default tick size. How many iterations are made in a tick */
 static int ticksize = 1;
 
+/* Information window */
 static WINDOW *info = NULL;
+/* Game area */
 static WINDOW *life = NULL;
 
+/* Holds the sleep time */
 static struct timespec sleeptime = {0};
 
+/* Prints the game area, but is not used since the curses interface. Obsolete
+ * */
 void print();
+
+/* Prints status information about the game. Window size, game size, tick size
+ * etc. Populates the info-window */
 void status();
+
+/* Initialize the game */
 void init();
+
+/* Initialize curses */
 void initcurses();
+
+/* Clean up the mess */
 void finish();
+
+/* Go forward a tick (ticksize iterations) */
 void tick();
+
+/* Activate a cell and the visual representation of it */
 void activate(int y, int x);
+
+/* A helper function for creating new windows */
 WINDOW *createwin(int height, int width, int begy, int begx);
+
+/* Keyboard utilities */
 bool kbd(int ch);
+
+/* Calculate the live neighbours for a cell */
 int neighbours(int y, int x);
 
 int main()
 {
-    int ch;
+    int ch; /* Holds the keyboard char */
     init();
 
     do
     { /* Keyboard loop */
         ch = getch();
     }
-    while(kbd(ch));
+    while(kbd(ch)); /* When the kbd returns false, we stop */
 
     finish();
 
@@ -47,108 +78,140 @@ int main()
 
 bool kbd(int ch)
 {
-    int x,y;
-    getyx(life, y, x);
+    int x,y; /* Holds the coordinates */
+    getyx(life, y, x); /* Get the coordinates from curses life window */
     switch(ch)
-    {
+    { /* hjkl + q + space + enter */
         case 'q':
             return false;
             break;
-        case 'l':
+        case 'l': /* Go right */
             x=(x+1)%CMAX;
             break;
-        case 'h':
+        case 'h': /* Go left */
             x=(x-1)%CMAX;
             break;
-        case 'j':
+        case 'j': /* Go down */
             y=(y+1)%LMAX;
             break;
-        case 'k':
+        case 'k': /* Go up */
             y=(y-1)%LMAX;
             break;
-        case ' ':
+        case ' ': /* Activate a cell */
             activate(y, x);
             break;
-        case 10: /* Enter */
+        case 10: /* Enter. Start the tick */
             tick();
             break;
-        case KEY_UP:
+        case KEY_UP: /* Increase tick size */
             ticksize++;
             status();
             break;
-        case KEY_DOWN:
+        case KEY_DOWN: /* Reduce tick size */
             ticksize--;
             status();
             break;
     }
-    wmove(life, y, x);
+    wmove(life, y, x); /* Move the coordinates to the new location and refresh
+                          the game area */
     wrefresh(life);
+
     return true;
 }
 
 void tick()
 {
-    struct timespec *remaining;
+    struct timespec *remaining; /* Holds the remaining time. Not used */
+    /* t = current tick iteration
+     * x = x coordinate
+     * y = y coordinate
+     * n = neighbours for a coordinate
+     */
     int t, x, y, n;
+    /* Sync the buffer with the game area
+     * buffer <- cells.
+     * The buffer and cells are separated so that the calculations from this
+     * round won't affect this round. For example let's imagine that coordinate
+     * (y,x) (1,1) is active but is deemed dead after visiting it. Then we
+     * check the coordinate (y,x) (2,1) which now has only 1 neighbour and also
+     * dies. By separating the cells and buffer we keep the cells untouchable
+     * during the round, but after we have finished the round, we can sync
+     * them. */
     COPYC;
     for(t = 0; t < ticksize; t++)
-    {
+    { /* Iterations */
         for(x = 0; x < lifecols; x++)
         {
             for(y = 0; y < lifelines; y++)
             {
+                /* Get the neighbours for the coordinate */
                 n = neighbours(y, x);
+                /* If a cell has less than two neighbours it dies, no matter
+                 * what. If a cell has more than three neighbours it's
+                 * overcrowded and dies too. */
                 if(n < 2 || n > 3)
                 {
                     ABCPR(y,x);
                     mvwaddch(life, y, x, ' ');
                 }
+                /* If a cell has 3 neighbours it revives */
                 if(n == 3)
                 {
                     BCPR(y, x);
                     mvwaddch(life, y, x, 'X');
                 }
+                /* The only possibility left is that the cell has two
+                 * neighbours, which means that the cell stays alive if it's
+                 * alive, if it's dead it stays dead. */
             }
         }
+        /* Sync the cells with the buffer */
         COPYB;
+        /* Sleep for while */
         nanosleep(&sleeptime, remaining);
+        /* Refresh the screen */
         wrefresh(life);
     }
 }
 
 int neighbours(int y, int x)
 {
-    int n = 0;
+    int n = 0; /* Alive neighbours */
+
+    /* The curses coordinate system puts y=0 to the top, so y-1 means checking
+     * for above and y+1 means the bottom */
+
+    /* Check for boundaries and then sum up the alive cells */
     if(y < LMAX)
     {
-        n += ALIVE(y+1,x);
+        n += ALIVE(y+1,x); /* Bottom */
     }
     if(y > 0)
     {
-        n += ALIVE(y-1,x);
+        n += ALIVE(y-1,x); /* Top */
     }
     if(x < CMAX)
     {
-        n += ALIVE(y, x+1);
+        n += ALIVE(y, x+1); /* Right */
         if(y > 0)
         {
-            n += ALIVE(y-1, x+1);
+            n += ALIVE(y-1, x+1); /* Top right */
         }
         if(y < LMAX)
         {
-            n += ALIVE(y+1, x+1);
+            n += ALIVE(y+1, x+1); /* Bottom right */
         }
     }
     if(x > 0)
     {
-        n += ALIVE(y, x-1);
+        n += ALIVE(y, x-1); /* Left */
         if(y > 0)
         {
-            n += ALIVE(y-1, x-1);
+            n += ALIVE(y-1, x-1); /* Top left */
         }
         if(y < LMAX)
         {
-            n += ALIVE(y+1, x-1);
+            n += ALIVE(y+1, x-1); /* Bottom left */
         }
     }
 
@@ -157,25 +220,30 @@ int neighbours(int y, int x)
 
 void finish()
 {
+    /* Free the cells and buffer */
     free(cells);
     free(buffer);
+    /* Delete the two windows we made */
     delwin(info);
     delwin(life);
+    /* And end curses mode. It's bad if this is forgotten. Vey very bad */
     endwin();
 }
 
 WINDOW *createwin(int height, int width, int begy, int begx)
 {
+    /* Hold the window */
     WINDOW *local_win = NULL;
 
+    /* Create the window */
     local_win = newwin(height, width, begy, begx);
-    if(local_win == NULL)
+    if(local_win == NULL) /* Check that it succeeded */
         exit(EXIT_FAILURE);
 
     wmove(local_win, 1, 1); /* Looks better if the cursor is not on
                                top of lines */
 
-    wrefresh(local_win);
+    wrefresh(local_win); /* And refresh */
 
     return local_win;
 }
@@ -184,40 +252,58 @@ void initcurses()
 {
     /* Initialize curses */
     initscr();
+    /* Don't echo keys */
     noecho();
     raw();
+    /* Allow arrows and other weird keys */
     keypad(stdscr, true);
+    /* Refresh the standard screen */
     refresh();
 
     lifecols = COLS-ICOLS-1; /* Leave one column empty */
     lifelines = LINES; /* Full height */
 
+    /* Create the two windows */
     life = createwin(lifelines, lifecols, 0, 0);
     info = createwin(ILINES, ICOLS, 0, lifecols+1);
+
+    /* Make a default box around the info-window */
     box(info, 0, 0);
+    /* Move, window, printw; Write to window info and move the cursor to 1,1
+     * before writing. */
     mvwprintw(info, 1,1, "INFO");
+
+    /* Move the cursor back to game area. The user don't need to see the cursor
+     * jumping to the info-window */
     wmove(life, 1,1);
+
+    /* Refresh both of the windows */
     wrefresh(info);
     wrefresh(life);
 }
 
 void init()
 {
+    /* Set the sleep time */
     sleeptime.tv_nsec = SLEEP;
+    /* Initialize the curses. Curses initialises few variables that we need
+     * further on */
     initcurses();
     /* Locale */
     setlocale(LC_ALL, "");
-    /* Initialize arrays */
+    /* Initialize arrays. SIZE is dependent on the life-window */
     cells = malloc(SIZE);
     if(cells == NULL)
         exit(1);
-    memset(cells, 0, SIZE);
+    memset(cells, 0, SIZE); /* Fill it with 0 */
 
+    /* The buffer is identical to cells */
     buffer = malloc(SIZE);
     if(buffer == NULL)
         exit(1);
     memset(buffer, 0, SIZE);
 
+    /* Write the first status information */
     status();
 }
 
@@ -259,6 +345,8 @@ void status()
 
 void activate(int y, int x)
 {
+    /* Set the cell alive in the array */
     CPR(y,x);
+    /* And show it visually too */
     mvwaddch(life, y, x, 'X');
 }
--- macros.h
+++ macros.h
@@ -1,25 +1,42 @@
 #ifndef LMACROS_H
 #define LMACROS_H
 
-# define LMAX (lifelines-1)
-# define CMAX (lifecols-1)
+# define LMAX (lifelines-1) /* Maximum lines */
+# define CMAX (lifecols-1) /* Maximum columns */
 
-# define SLEEP 250000000 /* Quarter of a second */
-# define ALIVE(y, x) (CELL((y), (x))==1)?1:0
-# define COORD(y, x) ((y)*lifecols+(x))
-# define ICOLS 30
-# define ILINES LINES
-# define ASIZE (lifecols*lifelines)
-# define SIZE (ASIZE*sizeof(int))
-# define CPR(y, x) (cells[COORD((y),(x))] = 1)
-# define BCPR(y, x) (buffer[COORD((y),(x))] = 1)
-# define ABCPR(y, x) (buffer[COORD((y),(x))] = 0)
-# define COPYB memcpy(cells, buffer, SIZE)
-# define COPYC memcpy(buffer, cells, SIZE)
-# define CELL(y, x) (cells[ COORD( (y), (x) ) ] )
+# define SLEEP 200000000 /* Fifth of a second */
+
+# define ALIVE(y, x) (CELL((y), (x))==1)?1:0 /* Check for whether a cell is
+                                                alive */
+
+# define COORD(y, x) ((y)*lifecols+(x)) /* Convert two-dimensional data to
+                                           one-dimensional data. y*xmax+x */
+
+# define ICOLS 30 /* Info-window width */
+# define ILINES LINES /* Info-window height */
+
+# define ASIZE (lifecols*lifelines) /* Amount of cells */
+# define SIZE (ASIZE*sizeof(int)) /* And the real size of it */
+
+# define CPR(y, x) (cells[COORD((y),(x))] = 1) /* Give CPR to the defined
+                                                  coordinate. (Make it alive)
+                                                  */
+# define BCPR(y, x) (buffer[COORD((y),(x))] = 1) /* The same but for buffer */
+# define ABCPR(y, x) (buffer[COORD((y),(x))] = 0) /* Kill the cell. Anticpr */
+
+# define COPYB memcpy(cells, buffer, SIZE) /* Sync the cells with the data from
+                                              the buffer */
+# define COPYC memcpy(buffer, cells, SIZE) /* Sync the buffer with the data
+                                              from the cells */
+
+# define CELL(y, x) (cells[ COORD( (y), (x) ) ] ) /* Data from the cell. */
+/* Debug macro */
 # define debugc(fmt, x, s) fprintf(stdout, "%s:%d %s=" fmt ": %s\n", __FILE__, __LINE__, #x, (x), (s))
+/* Helper macro for debugging */
 # define debug(fmt, x) debugc(fmt, x, "")
+
+/* Curses defines TRUE and FALSE, but vim colours true and false (lowercase) */
 #define true 1
 #define false 0
 
-#endif
+#endif 
